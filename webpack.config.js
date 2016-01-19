02c70dce2e3168826b00545c1e69589860e85ecf
Gauri Shankar P <gauri.gshankar@yahoo.co.in>
2016-01-18 18:57:48 -0800

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015' , 'react']
+        }
+      }
+    ]
+  }
+};
